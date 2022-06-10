a70d8ddecd9e93872d7c1032bd48d1dbfba5d0fc
firwanaa <firwanaa@sheridancollege.ca>
2022-06-09 21:53:51 -0400

    even more optimization

--- code.hs
+++ code.hs
@@ -11,11 +11,11 @@ instance Show Op where
   show Mul = "*"
   show Div = "/"
 
-valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
-valid Sub x y = x > y
-valid Mul _ _ = True
-valid Div x y = x `mod` y == 0
+-- valid :: Op -> Int -> Int -> Bool
+-- valid Add _ _ = True
+-- valid Sub x y = x > y
+-- valid Mul _ _ = True
+-- valid Div x y = x `mod` y ==p 0
 
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
@@ -128,5 +128,36 @@ ops = [Add, Sub, Mul, Div]
 solutions :: [Int] -> Int -> [Expr]
 solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
 
+type Result = (Expr, Int)
+
+results :: [Int] -> [Result]
+results [] = []
+results [n] = [(Val n,n) | n > 0]
+results ns = [res | (ls,rs) <- split ns,
+                     lx     <- results ls,
+                     ry     <- results rs,
+                     res    <- combine' lx ry]
+
+combine' :: Result -> Result -> [Result]
+combine' (l,x) (r,y) = [(App o l r, apply o x y) | o <- ops, valid o x y]
+
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n = [e | ns' <- choices ns, (e,m) <- results ns', m == n]
+
+-- x + y == y + x
+-- x * y == y * x
+-- x * 1 == x
+-- 1 * y == y
+-- x / 1 == x
+
+-- modify valid function
+valid :: Op -> Int -> Int -> Bool
+valid Add x y = x <=y
+valid Sub x y = x > y
+valid Mul x y = x /= 1 && y/= 1 && x <= y
+valid Div x y = y /= 1 && x `mod` y == 0
+
 main :: IO ()
-main = print (solutions [1,3,7,10,25,50] 765)
+-- main = print (solutions [1,3,7,10,25,50] 765) -- will take longer time
+main = print (solutions' [1,3,7,10,25,50] 765) -- fast
